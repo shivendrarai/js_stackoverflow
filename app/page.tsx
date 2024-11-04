65a52879184f08cb8f96926fb3e35a41108ee236
shivendrarai <deritshivendra@gmail.com>
2024-11-04 20:24:59 +0530

    Asset and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 const Home = () => {
   return (
     <>
-      <h1 className="text-3xl   font-black text-primary-500">
-        Welcome to Next.js
-      </h1>
+      <h1 className="h1-bold">Welcome to Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Next.js</h1>
     </>
   );
 };
